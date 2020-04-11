c52441766048190142c2d5af2af0ee0a9a42786e
Nicolas Tapiero <tapiero@sas.upenn.edu>
2020-04-10 20:52:51 -0400

    added first screen

---
+++ gui.py
@@ -0,0 +1,61 @@
+import tkinter
+from tkinter import messagebox
+from tkinter import *
+
+win = Tk()
+
+top = Frame(win, borderwidth=2, relief="solid")
+bottom = Frame(win, borderwidth=2, relief="solid")
+
+top.pack(side="top", expand=True, fill="both")
+bottom.pack(side="bottom", expand=True, fill="both")
+
+entries = [Entry(top)]
+
+for entry in entries:
+    entry.pack()
+
+def cont():
+    #print(w.get())
+    for entry in entries:
+        nums = entry.get().split(',')
+        bigbreak = False
+        print(nums)
+        for num in nums:
+            num = num.lstrip().strip()
+            if (not str.isdigit(num)) or num == '':
+                bigbreak = True
+                break
+        if bigbreak:
+            break
+    else:
+        top.pack_forget()
+        bottom.pack_forget()
+        return
+
+    messagebox.showerror("Error","All boxes must have numbers separated by commas to represent a clause")
+
+def addThing():
+    local_entry = Entry(top)
+    entries.append(local_entry)
+    local_entry.pack()
+
+def removeThing():
+    local_entry = entries[-1]
+    local_entry.pack_forget()
+    entries.pop()
+
+button = Button(bottom, text="Add New Clause", command=addThing)
+button.pack()
+
+button = Button(bottom, text="Remove Bottom Clause", command=removeThing)
+button.pack()
+
+button = Button(bottom, text="Submit Formula", command=cont)
+button.pack()
+
+
+win.title("PennSAT Visualizer")
+win.mainloop()
+
+
