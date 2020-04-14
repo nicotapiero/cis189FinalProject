ef1802581ad037e8dfa92a2a8dba1e9835b82665
Nicolas Tapiero <tapiero@sas.upenn.edu>
2020-04-14 19:24:25 -0400

    fixed the stupid quadrands

--- solver_phase.py
+++ solver_phase.py
@@ -9,38 +9,51 @@ from pennSAT import *
 
 
 def solver_phase(max, cnf, win):
-    win.grid_rowconfigure(1, weight=1)
-    win.grid_columnconfigure(1, weight=1)
+    # win.grid_rowconfigure(1, weight=1)
+    # win.grid_columnconfigure(1, weight=1)
 
     #
-    left_frame = Frame(win, width=500, height=150, background="black").pack(side="left", fill=BOTH)
-    right_frame = Frame(win, width=500, height=150, background="black").pack(side="right", fill=BOTH)
+    # left_frame = Frame(win, width=500, height=150, background="black").pack(side="left", fill=BOTH)
+    # right_frame = Frame(win, width=500, height=150, background="black").pack(side="right", fill=BOTH)
 
-    top_left = Frame(left_frame, width=500, height=150, background="red").pack(fill="both", expand=True, padx=20, pady=20)
-    top_right = Frame(right_frame, width=500, height=150, background="blue").pack(fill="both", expand=True, padx=20, pady=20)
-    bottom_left = Frame(left_frame, width=500, height=150, background="green").pack(fill="both", expand=True, padx=20, pady=20)
-    bottom_right = Frame(right_frame, width=500, height=150, background="yellow").pack(fill="both", expand=True, padx=20, pady=20)
-    #
-    # # top_left
-    # # top_right
-    # # bottom_left
-    # # bottom_right
-    #
+    # top_left = Frame(win, width=500, height=150, background="red").pack(fill="both", expand=True, padx=20, pady=20)
+    # top_right = Frame(win, width=500, height=150, background="blue").pack(fill="both", expand=True, padx=20, pady=20)
+    # bottom_left = Frame(win, width=500, height=150, background="green").pack(fill="both", expand=True, padx=20, pady=20)
+    # bottom_right = Frame(win, width=500, height=150, background="yellow").pack(fill="both", expand=True, padx=20, pady=20)
+
+    top_left = Frame(win, width=500, height=150, background="red")
+    top_left.grid(row=0, column=0)
+    top_right = Frame(win, width=500, height=150, background="blue")
+    top_right.grid(row=1, column=0)
+    bottom_left = Frame(win, width=500, height=150, background="green")
+    bottom_left.grid(row=0, column=1)
+    bottom_right = Frame(win, width=500, height=150, background="yellow")
+    bottom_right.grid(row=1, column=1)
+    # #
+    # # # top_left
+    # # # top_right
+    # # # bottom_left
+    # # # bottom_right
+    # #
     text = StringVar()
-
-
+    #
+    #
     print(max, cnf)
     solver = PennSAT(max, cnf, True)
 
     text.set(str(solver.assignment_stack))
-    label = Label(textvariable=text).grid(row=0, column=0)
+    label = Label(top_left,textvariable=text)
+    label.pack()
 
     # label.pack()
 
-    ass_stack_label = Label(top_left, text="Assignment Stack Stff").grid(row=0, column=0)
+    ass_stack_label = Label(top_left, text="Assignment Stack Stff")
+    ass_stack_label.pack()
 
-    tree_label = Label(bottom_right, text="Tree Stuff").grid(row=0, column=0)
-    flowchart_label = Label(top_right, text="Flowchart").grid(row=0, column=0)
+    tree_label = Label(bottom_left, text="Tree Stuff")
+    tree_label.pack()
+    flowchart_label = Label(top_right, text="Flowchart")
+    flowchart_label.pack()
 
     # ass_stack = Label()
     # ass_stack["text"] = solver.assignment_stack
@@ -54,6 +67,7 @@ def solver_phase(max, cnf, win):
 
         text.set(str(solver.assignment_stack))
 
-    control_label = Label(bottom_right, text="Controls").grid(row=0, column=0)
-    button = Button(bottom_right, text="start").grid(row=0, column=0)
-    # button.pack(side="bottom")
+    control_label = Label(bottom_right, text="Controls")
+    control_label.pack()
+    button = Button(bottom_right, text="start", command=start)
+    button.pack()
